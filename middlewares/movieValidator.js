4be933438ae8bbe32db30448793f06dee85e9632
OlgaKG <flory_oli@mail.ru>
2023-10-11 09:51:07 +0200

    validation

--- middlewares/movieValidator.js
+++ middlewares/movieValidator.js
@@ -4,15 +4,16 @@ const urlSchema = Joi.string().pattern(/(?:https?):\/\/(w{3}\.)?\w+([.|-]{1,}\w+
 
 module.exports.createMovieValidator = celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(2).max(30),
-    director: Joi.string().required().min(2).max(30),
+    country: Joi.string().required().min(2).max(100),
+    director: Joi.string().required().min(2).max(70),
     duration: Joi.number().required().min(1),
     year: Joi.string().required().min(4).max(4),
     description: Joi.string().required(),
     image: urlSchema.required(),
     trailerLink: urlSchema.required(),
     thumbnail: urlSchema.required(),
-    movieId: Joi.number().required(),
+    movieId: Joi.number(),
+    // movieId: Joi.number().required(),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
   }),
